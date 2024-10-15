4f766f8d6b548ef5f6a29a20829a4a7fd4ebfaf2
shotoyaar <anirudhsridhar1064@gmail.com>
2024-10-15 19:54:44 +0530

    feat: initial setup with nuxt modules->image,content,tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="main-container">
+      <p>what the what the what the what the</p>
+    </div>
 </template>
+
+<style is:global>
+  .main-container {
+    background-color: #242424;
+    color: #499393;
+    text-align: center;
+    min-height: 300px;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
