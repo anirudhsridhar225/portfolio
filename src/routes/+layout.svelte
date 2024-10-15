29bdf1c2bd597f3369ef30c234e5dc443d71ad1d
shotoyaar <anirudhsridhar1064@gmail.com>
2024-10-16 01:21:18 +0530

    some basic content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,14 @@
 	import Header from './Header.svelte';
 </script>
 
-<div class="app">
+<div class="bg-[#E5DFC5] min-h-screen">
 	<Header />
 
 	<main>
 		<slot />
 	</main>
 
-	<footer class="w-full text-center bg-white p-6">
+	<footer class="w-full text-center bg-[#B9C0AB] p-4">
 		<p class=" text-xl font-semibold">(somehow) made by andy</p>
 	</footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	</script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>My Portfolio</title>
+	<meta name="andy-portfolio" content="Andy's Potfolio Website" />
 </svelte:head>
 
-<section>
-  <h1 class="bg-blue-600 p-8 m-8 text-red-300 font-mono">what's up</h1>
+<section class="min-h-screen">
+  <p class="p-8 pb-0 text-[#5C6A72] font-mono text-6xl">what's up</p>
+  <p class="p-8 pt-3 text-[#5C6A72] font-mono text-2xl">how are ya</p>
 </section>
