ff9620f4a5258e0a98e3c30adbe7e35cd230d5be
root <root@DESKTOP-EK087PV.localdomain>
2024-12-11 19:25:45 +0000

    i made it italic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,5 +8,5 @@
 
 <section class="min-h-screen">
   <p class="p-8 pb-0 text-[#5C6A72] font-mono text-6xl">what's up</p>
-  <p class="p-8 pt-3 text-[#5C6A72] font-mono text-2xl">how are ya</p>
+  <p class="p-8 pt-3 text-[#5C6A72] font-mono text-2xl italic">how are ya</p>
 </section>
