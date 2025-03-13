54375660af2b15c95114e858525b2ebd205405ea
shotoyaar <anirudhsridhar1064@gmail.com>
2025-03-13 23:52:47 +0530

    new svelte with deno yay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,2 @@
-<script lang="ts">
-</script>
-
-<svelte:head>
-	<title>My Portfolio</title>
-	<meta name="andy-portfolio" content="Andy's Potfolio Website" />
-</svelte:head>
-
-<section class="m-4">
-	<section class="h-full">
-		<p class="p-4 pt-0 text-[#458588] font-mono text-4xl italic tracking-tighter">$(whoami)?</p>
-		<p class="p-4 pt-0 text-[#282828] font-mono text-2xl tracking-tight">I'm Andy, a rookie web-developer based off Chennai, India.</p>
-	</section>
-</section>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
