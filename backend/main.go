415133267fdbf6070dcd7c9e80d4ac67f2533a2c
anirudhsridhar225 <anirudhsridhar1064@gmail.com>
2025-04-28 00:42:55 +0530

    [FEAT]: initial htmx, go and sqlc commit

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"context"
+	"log"
+	"net/http"
+	"html/template"
+	"strconv"
+	"strings"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	ctx := context.Background()
+}
